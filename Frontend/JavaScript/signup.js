532b151d55ece677d1da12962470655ca597cf32
Balram <bkushwah4321@gmail.com>
2025-12-01 21:07:50 +0530

    funcion added by Balram Kushwah

--- Frontend/JavaScript/signup.js
+++ Frontend/JavaScript/signup.js
@@ -1,14 +1,7 @@
-function submit_form(){
-    let name=document.getElementById("name").value;
-    let mobile=document.getElementById("number").value;
-    let email=document.getElementById("email").value;
-
-    fetch("http://127.0.0.1:5000/signup", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({ name, mobile, email })
-    })
-    .then(res => res.json())
-    .then(data => alert("OTP Sent: " + data.otp))
-    .catch(err => console.error(err));
-}
+ function submit_form(){
+        let name = document.getElementById("name").value;
+        let mobile = document.getElementById("mobile").value;
+        let email = document.getElementById("email").value;
+        let res = document.querySelector(".result");
+        res.innerText = "Balram";
+    }
