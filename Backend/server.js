c1083848f63769933f2666de10ff952a8eabbf22
Balram <bkushwah4321@gmail.com>
2025-12-12 20:16:14 +0530

    balram

--- Backend/server.js
+++ Backend/server.js
@@ -6,11 +6,19 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://balramkushwah21.github.io/E-Commerce/", // your actual GitHub Pages URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/", authRoutes);
 
-app.listen(3050, () =>{
-    console.log("Server running on port 3050");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running:", PORT));
+
+
+
+
+
