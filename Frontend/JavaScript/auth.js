9012bc1225e138e4c635ef2ef12b641c00a215b8
Balram <bkushwah4321@gmail.com>
2025-12-26 22:35:05 +0530

    Balram

--- Frontend/JavaScript/auth.js
+++ Frontend/JavaScript/auth.js
@@ -45,8 +45,7 @@ if (loginForm) {
     const res = await fetch("https://e-commerce-pgvp.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({email: "test@test.com",
-    password: "123456"})
+      body: JSON.stringify(data)
     });
     const result = await res.json();
       if(result.message === "Login successful") {
